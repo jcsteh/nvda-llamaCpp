43bc16f0f277b1992db1276472c6be89bf7a7c19
Beka Gozalishvili <beqaprogger@gmail.com>
2023-12-02 17:14:57 +0400

    Add gui Add config spec Replace urllib with http.client

--- addon/globalPlugins/llamaCpp/__init__.py
+++ addon/globalPlugins/llamaCpp/__init__.py
@@ -2,20 +2,21 @@
 # License: GNU General Public License
 
 import base64
+import http.client
 import io
 import json
 import os
 import sys
-import tempfile
 import threading
-import urllib.parse
-import urllib.request
 import wx
 
+import addonHandler
 import api
 import config
 import globalPluginHandler
 import gui
+from gui import guiHelper, nvdaControls
+from gui.settingsDialogs import SettingsPanel
 import queueHandler
 import screenBitmap
 import speech
@@ -23,18 +24,54 @@ import ui
 from logHandler import log
 from scriptHandler import script
 
-DEFAULT_URL = "http://localhost:8080/"
+DEFAULT_HOST = "localhost"
+DEFAULT_PORT = 8080
 PROMPT = "This is a conversation between User and Llama, a friendly chatbot. Llama is helpful, kind, honest, good at writing, and never fails to answer any requests immediately and with precision. Llama is especially good at describing images in great detail for users who can't see.\nUSER: [img-10] Please describe this image in detail.\nASSISTANT:"
 # This can take a long time, particularly if running on the CPU.
-TIMEOUT = 180
+DEFAULT_TIMEOUT = 180
+
+_curAddon = addonHandler.getCodeAddon()
+addonName = _curAddon.name.lower()
+_addonSummary = _curAddon.manifest['summary']
+addonHandler.initTranslation()
+
+confspec = {
+	"host": f"string(default={DEFAULT_HOST})",
+	"port": f"integer(default={DEFAULT_PORT})",
+	"timeout": f"integer(default={DEFAULT_TIMEOUT})"
+}
+config.conf.spec[addonName] = confspec
+
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	scriptCategory = _addonSummary
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.config = config.conf[addonName]
+		LlamaCppSettingsPanel.config = self.config
+		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(LlamaCppSettingsPanel)
+
+	def terminate(self):
+		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(LlamaCppSettingsPanel)
 
 	@script(
 		gesture="kb:NVDA+shift+l",
-		description="Recognizes the content of the current navigator object with llama.cpp."
+		description=_("Recognizes the content of the current navigator object with llama.cpp.")
 	)
 	def script_recognizeWithLlamaCpp(self, gesture):
+		self._imgData = self.takeScreenshot()
+		ui.message(_("Recognizing"))
+		# Maintain a history of the conversation, as we have to re-send this with
+		# each subsequent query.
+		self._history = PROMPT
+		# Create the dialog so the background thread can reference methods on it, but
+		# don't show it yet. The dialog will show itself when the first token of the
+		# response is provided.
+		self._dialog = ResultDialog(self)
+		self._query()
+
+	def takeScreenshot(self):
 		location = api.getNavigatorObject().location
 		sb = screenBitmap.ScreenBitmap(location.width, location.height)
 		pixels = sb.captureImage(
@@ -45,25 +82,8 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		img = img.ConvertToImage()
 		stream = io.BytesIO()
 		img.SaveFile(stream, wx.BITMAP_TYPE_JPEG)
-		imgData = base64.b64encode(stream.getvalue())
-		if (
-			(confSect := config.conf.get("llamaCpp")) and
-			(confUrl := confSect.get("url"))
-		):
-			self._url = confUrl
-		else:
-			self._url = DEFAULT_URL
-		self._url = urllib.parse.urljoin(self._url, "completion")
-		self._imgData = imgData.decode("UTF-8")
-		ui.message("Recognizing")
-		# Maintain a history of the conversation, as we have to re-send this with
-		# each subsequent query.
-		self._history = PROMPT
-		# Create the dialog so the background thread can reference methods on it, but
-		# don't show it yet. The dialog will show itself when the first token of the
-		# response is provided.
-		self._dialog = ResultDialog(self)
-		self._query()
+		imgData = base64.b64encode(stream.getvalue()).decode("UTF-8")
+		return imgData
 
 	def _query(self):
 		# The web request is synchronous, so run it in a thread.
@@ -72,19 +92,21 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def _bgQuery(self):
 		try:
-			req = urllib.request.Request(
-				self._url,
-				data=json.dumps({
-					"prompt": self._history,
-					"stream": True,
-					"image_data": [
-						{"id": 10, "data": self._imgData}
-					],
-				}).encode(),
-				headers={"Content-Type": "application/json"}
-			)
-			resp = urllib.request.urlopen(req, timeout=TIMEOUT)
-			for token in resp:
+			host = self.config["host"]
+			port = self.config["port"]
+			timeout = self.config["timeout"]
+			connection = http.client.HTTPConnection(host, port, timeout=timeout)
+			data = json.dumps({
+				"prompt": self._history,
+				"stream": True,
+				"image_data": [
+					{"id": 10, "data": self._imgData}
+				],
+			})
+			headers = {"Content-Type": "application/json"}
+			connection.request("POST", "/completion", body=data, headers=headers)
+			response = connection.getresponse()
+			for token in response:
 				if self._thread is not threading.current_thread():
 					# This previous request was cancelled.
 					return
@@ -102,12 +124,14 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			wx.CallAfter(self._dialog.responseDone)
 		except Exception:
 			log.exception("")
+		finally:
+			connection.close()
 
 	def _send(self, query):
 		"""Called by the dialog when the user sends a follow-up query.
 		"""
 		self._history += "\nUSER: %s\nASSISTANT:" % query
-		ui.message("Please wait")
+		ui.message(_("Please wait"))
 		self._query()
 
 	def _finish(self):
@@ -186,3 +210,39 @@ class ResultDialog(wx.Dialog):
 		self.plugin._finish()
 		self.plugin = None
 		self.Destroy()
+
+
+class LlamaCppSettingsPanel(SettingsPanel):
+    # Translators: name of the dialog.
+	title = "LlamaCpp"
+
+	def makeSettings(self, sizer):
+		sHelper = gui.guiHelper.BoxSizerHelper(self, sizer=sizer)
+		# Translators: A setting in addon settings dialog.
+		hostLb = _("&Host address")
+		self.hostEdit = sHelper.addLabeledControl(hostLb, wx.TextCtrl)
+		self.hostEdit.Value=self.config["host"]
+        # Translators: A setting in addon settings dialog.
+		portLb = _("&Port")
+		self.portSpin = sHelper.addLabeledControl(
+			portLb,
+			nvdaControls.SelectOnFocusSpinCtrl,
+			min=0,
+			max=65353,
+			initial=self.config["port"])
+        # Translators: A setting in addon settings dialog, shown if source language is on auto.
+		timeoutLb = _("&Timeout")
+		self.timeoutSpin = sHelper.addLabeledControl(
+			timeoutLb,
+			nvdaControls.SelectOnFocusSpinCtrl,
+			min=10,
+			max=3600,
+			initial=self.config["timeout"])
+
+	def postInit(self):
+		self.hostEdit.SetFocus()
+
+	def onSave(self):
+		self.config["host"] = self.hostEdit.Value
+		self.config["port"] = self.portSpin.Value
+		self.config["timeout"] = self.timeoutSpin.Value
